f27b4f294201202411b8fb565600c6aba7be6c8e
Анжелика Грибова <pm14.gribova@gmail.com>
2026-04-28 21:52:07 +0700

    add UserController

---
+++ src/main/java/ru/angelika/boutique/controller/rest/UserRestController.java
@@ -0,0 +1,26 @@
+package ru.angelika.boutique.controller.rest;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.angelika.boutique.dto.UserDto;
+import ru.angelika.boutique.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserRestController {
+    private final UserService userService;
+
+    @Autowired
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public void addUser(@Valid @RequestBody UserDto user) {
+        userService.addUser(user);
+    }
+}
