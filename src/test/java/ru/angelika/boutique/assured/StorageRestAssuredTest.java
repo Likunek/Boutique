715dc3ad19469718002a84167c1c08eb5291af56
Анжелика Грибова <pm14.gribova@gmail.com>
2026-05-30 17:26:36 +0700

    added profile for tests

--- src/test/java/ru/angelika/boutique/assured/StorageRestAssuredTest.java
+++ src/test/java/ru/angelika/boutique/assured/StorageRestAssuredTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.angelika.boutique.dto.StorageDto;
@@ -25,6 +26,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 class StorageRestAssuredTest {
 
